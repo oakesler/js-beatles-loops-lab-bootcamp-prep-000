a1df2ab329e035057c04259379591091356d53b0
Olivia Kesler <oakesler@protonmail.ch>
2018-12-21 17:42:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const empty = [];
+  var i = 0; 
+  var x = musicians.length;
+  for (i = 0; i < x ; i++) {
+    empty.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return empty;
+}
+
+ 
+function johnLennonFacts(array) {
+ 
+  var a = 0;
+  var facts = [];
+  
+  while (array.length > facts.length) {
+    
+    facts.push(`${array[a]}!!!`);
+    a++;
+    }
+  return facts; 
+}
+
+function iLoveTheBeatles (n) {
+  var emptyish = [];
+  
+  do emptyish.push("I love the Beatles!");
+     while (emptyish.length > n); {
+      }
+  return emptyish;
+}
+  
